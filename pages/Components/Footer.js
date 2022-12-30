ba8c6ff05ad505feb6b9a5bb2f301a2a00fb4e1a
Dasuni Charuka <dasunicharuka4@gmail.com>
2022-12-30 20:08:02 +0530

    new

---
+++ pages/Components/Footer.js
@@ -0,0 +1,69 @@
+import React from 'react';
+import Link from 'next/link';
+import { SocialIcon } from 'react-social-icons';
+
+function Footer() {
+  return (
+    <div className='footer-container'>
+      <section className='footer-a'>
+        <p className='footer-a-heading'>
+          dnjkesbfuye ferugfeui fbeurf bchsdbufye ncndiuhfw ncsiaudb cnsdbidu.
+        </p>
+      </section>
+      <div class='footer-links'>
+        <div className='footer-link-wrapper'>
+          <div class='footer-link-items'>
+            <Link
+              href="/"
+            >
+              HOME
+            </Link>
+          </div>
+          <div class='footer-link-items'>
+            <Link
+              href="/Components/Services"
+            >
+              SERVICES
+            </Link>
+          </div>
+          <div class='footer-link-items'>
+            <Link
+              href="/"
+            >
+              CONDITIONS
+            </Link>
+          </div>
+          <div class='footer-link-items'>
+            <Link
+             href="/Components/contactus"
+            >
+              CONTACT
+            </Link>
+          </div>
+          <div class='footer-link-items'>
+            <Link
+              href="/Components/about-us"
+            >
+              ABOUT
+            </Link>
+          </div>
+        </div>
+      </div>
+      <section class='social-media'>
+        <div class='social-media-wrap'>
+          <div class='social-icons'>
+          <SocialIcon  class='social-icon-link' url="https://facebook.com" />
+          &nbsp;&nbsp;
+          <SocialIcon url="https://instagram.com" />
+          &nbsp;&nbsp;
+          <SocialIcon url="https://twitter.com" />
+          &nbsp;&nbsp;
+          <SocialIcon url="https://linkedin.com" />
+          </div>
+        </div>
+      </section>
+    </div>
+  );
+}
+
+export default Footer;
