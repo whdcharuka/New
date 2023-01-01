ba2364d0cb0b993a2a1baa8c0336e8d5c7d596aa
Dasuni Charuka <dasunicharuka4@gmail.com>
2023-01-01 21:28:40 +0530

    modified

--- pages/Components/home/home-page.jsx
+++ pages/Components/home/home-page.jsx
@@ -1,172 +1,77 @@
-import Link from 'next/link';
-import Image from 'next/image';
+import Image from 'next/image'
+import Head from 'next/head';
+import service from '/public/images/Customer-Service.jpg'
+import styles from '/styles/Services.module.css'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faArchive, faHeadphones, faThumbsUp, faTruck } from "@fortawesome/free-solid-svg-icons";
+import Navbar from '../Navbar/Navbar';
+import Footer from '../Footer';
 
-export const HomePage = () => (
-  <>
-  <h5>Browse items by categories</h5>
+const Services = () =>{
+    
+  return (
+    <div>
+        <Head><title>Terms & Conditions</title></Head>
+        <Navbar/>
+    <section className={styles.bg1}>
+   
+        <h1 className={styles.text1}>Terms & Conditions</h1>
+    <Image
+      src={service}
+      className={styles.image}
+    />
+   
+        <div className='container'>
+            <div className='row'>
+                
+                <div className='col-lg-3 mb-2'>
+                    <div className='card h-100  bg-light border-0'>
+                        <div className='card-body'>
+                            <i><FontAwesomeIcon icon={faTruck} /></i>
+                            <h5>Fast Delivery</h5>
+                            <p>We provide lslandwide delivery within 4-7 days.</p>
+                        </div>
+                    </div>
+
+                </div>
+                <div className='col-lg-3 mb-2'>
+                    <div className='card h-100  bg-light border-0 '>
+                        <div className='card-body'>
+                           <i> <FontAwesomeIcon icon={faArchive} /></i>
+                            <h5>Quality Product</h5>
+                            <p>We sell genuine products with company authorized.</p>
+                        </div>
+                    </div>
+
+                </div>
+                <div className='col-lg-3 mb-2'>
+                    <div className='card h-100  bg-light border-0'>
+                        <div className='card-body'>
+                            <i><FontAwesomeIcon icon={faThumbsUp} /></i>
+                            <h5>Shop With Confidence </h5>
+                            <p>Confident and active online services.</p>
+                        </div>
+                    </div>
+
+                </div>
+                <div className='col-lg-3 mb-2'>
+                    <div className='card h-100  bg-light border-0'>
+                        <div className='card-body'>
+                            <i><FontAwesomeIcon icon={faHeadphones} /></i>
+                            <h5>24/7 Support</h5>
+                            <p>Call our hotlines during office hours for any questions or inquiries.</p>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    </section>
+    <div className={styles.servicesfooter}>
+        <Footer/>
+      </div>
+    </div>
+  )   
+  }
   
-<ul className='hlist'>
-  <li className='hlist1'>
-          <div className="image">
-            <Image width={60} height={60} alt='' src='/images/categories/car.png'/><Link href="/" passHref><a>Vehical Parts</a></Link>
-          </div>
-          </li>
-          <li className='hlist1'>
-          <div className="image">
-            <Image width={60} height={60} alt='' src='/images/categories/cat.png'/><Link href="/" passHref><a>Animals</a></Link>
-          </div>
-          </li>
-          <li className='hlist1'>
-          <div className="image">
-            <Image width={60} height={60} alt='' src='/images/categories/burger.png'/><Link href="/" passHref><a>Foods</a></Link>
-          </div>
-          </li>
-          <li className='hlist1'>
-          <div className="image">
-            <Image width={60} height={60} alt='' src='/images/categories/education.png'/><Link href="/" passHref><a>Education</a></Link>
-          </div>
-          </li>
-          </ul>
-
-          <ul className='hlist'>
-          <li className='hlist1'>
-          <div className="image">
-            <Image width={60} height={60} alt='' src='/images/categories/smartphone (2).png'/><Link href="/" passHref><a>Electronics</a></Link>
-          </div>
-          </li>
-          <li className='hlist1'>
-          <div className="image">
-            <Image width={60} height={60} alt='' src='/images/categories/giftbox.png'/><Link href="/" passHref><a>Gifts</a></Link>
-          </div>
-          </li>
-          <li className='hlist1'>
-          <div className="image">
-            <Image width={60} height={60} alt='' src='/images/categories/house.png'/><Link href="/" passHref><a>Home</a></Link>
-          </div>
-          </li>
-          <li className='hlist1'>
-          <div className="image">
-            <Image width={60} height={60} alt='' src='/images/categories/sports.png'/><Link href="/" passHref><a>Sports</a></Link>
-          </div>
-          </li>
-          </ul>
-
-          <ul className='hlist'>
-          <li className='hlist1'>
-          <div className="image">
-            <Image width={60} height={60} alt='' src='/images/categories/essential-oil.png'/><Link href="/" passHref><a>Essential Item</a></Link>
-          </div>
-          </li>
-          <li className='hlist1'>
-          <div className="image">
-            <Image width={60} height={60} alt='' src='/images/categories/toys.png'/><Link href="/" passHref><a>Toys</a></Link>
-          </div>
-          </li>
-          <li className='hlist1'>
-          <div className="image">
-            <Image width={60} height={60} alt='' src='/images/categories/guitar.png'/><Link href="/" passHref><a>Music</a></Link>
-          </div>
-          </li>
-          <li className='hlist1'>
-          <div className="image">
-            <Image width={60} height={60} alt='' src='/images/categories/dress.png'/><Link href="/" passHref><a>Fashion</a></Link>
-          </div>
-          </li>
-          </ul>
-  <h5>How it works</h5>
-
-  <a>Create free account</a><br></br>
-  <p>cdscsdvs</p>
-  <a>Create free account</a><br></br>
-  <a>Create free account</a><br></br>
-  </>
-);
-
-
-// const Wrapper = () => {
-//   const data = [
-//     {
-//       cateImg: "./images/Categories/car.png",
-//       title: "Vehical Parts",
-//       path:"./Vehicalparts",
-//     },
-//     {
-//       cateImg: "./images/Categories/smartphone (2).png",
-//       title: "Electronics",
-//       path:"./ElectronicDevices",
-//     },
-//     {
-//       cateImg: "./images/Categories/house.png",
-//       title: "Home & Garden",
-//       path:"./HomeGarden",
-//     },
-//     {
-//       cateImg: "./images/Categories/cat.png",
-//       title: "Pets",
-//       path:"./Animals",
-//     },
-//     {
-//       cateImg: "./images/Categories/toys.png",
-//       title: "Toys",
-//       path:"/Toys",
-//     },
-//     {
-//       cateImg: "./images/Categories/sports.png",
-//       title: "Hobby & Sport",
-//       path:"./HobbySport",
-//     },
-//     {
-//       cateImg: "./images/Categories/burger.png",
-//       title: "Food",
-//       path:"./Foods",
-//     },
-//     {
-//       cateImg: "./images/Categories/education.png",
-//       title: "Education",
-//       path:"./Education",
-//     },
-//     {
-//       cateImg: "./images/Categories/essential-oil.png",
-//       title: "Essentials",
-//       path:"./Essentials",
-//     },
-//     {
-//       cateImg: "./images/Categories/dress.png",
-//       title: "Fashion & Beauty",
-//       path:"./FashionBeauty",
-//     },
-//     {
-//       cateImg: "./images/Categories/giftbox.png",
-//       title: "Gifts",
-//       path:"./Gifts",
-//     },
-//     {
-//       cateImg: "./images/Categories/guitar.png",
-//       title: "Music",
-//       path:"./Music",
-//     },
-//   ]
-//   return (
-//     <>
-//       <section className='wrapper123 wbackground123'>
-//       <h1 className="how1">Browse items by category</h1>
-//         <div className='wcontainer grid5'>
-//           {data.map((val, index) => {
-//             return (
-//               <div key={index}>
-                
-//                 <div className="category1">
-//                  <img src={val.cateImg} alt='' />
-//                 </div>
-//                 <span>{val.title}</span>
-                
-//               </div>
-//             )
-//           })}
-//         </div>
-//       </section>
-//     </>
-//   )
-// }
-
-// export default Wrapper
+  export default Services
