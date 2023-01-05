37d87747cdd8f2e07a0218a7962429ca6ae7a29e
Dasuni Charuka <dasunicharuka4@gmail.com>
2023-01-05 07:33:55 +0530

    modified

--- pages/Components/vehical/VpCategories.js
+++ pages/Components/vehical/VpCategories.js
@@ -3,7 +3,7 @@ import Head from 'next/head';
 import service from '/public/images/Customer-Service.jpg'
 import styles from '/styles/Services.module.css'
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faArchive, faHeadphones, faThumbsUp, faTruck } from "@fortawesome/free-solid-svg-icons";
+import { faArchive, faBox, faHeadphones, faSquare, faThumbsUp, faTruck, } from "@fortawesome/free-solid-svg-icons";
 import Navbar from '../Navbar/Navbar';
 import Footer from '../Footer';
 import Link from 'next/link';
@@ -14,13 +14,13 @@ const Categories = () => {
     <>
       <div className='vcategory'>
       <h3 className='types'>TYPES</h3>
-        <div className='box '>
-          <ul>
-            <Link href="./Engine"><i class="fa fa-square" aria-hidden="true"><span>ENGINE</span></i></Link>
-            <Link href="./Battery"><i class="fa fa-square" aria-hidden="true"><span>BATERY</span></i></Link>
-            <Link href="./Wheel"><i class="fa fa-square" aria-hidden="true"><span>WHEEL</span></i></Link>
-            <Link href="./Fuel"><i class="fa fa-square" aria-hidden="true"><span>FUEL TANK</span></i></Link>
-            <Link href="./Screen"><i class="fa fa-square" aria-hidden="true"><span>WINDSCREEN</span></i></Link>
+        <div className='box'>
+          <ul className="rty2">
+            <Link href="./Engine" className="rty1" ><i> <FontAwesomeIcon icon={faSquare} className="rty" /><span className='cvb'>ENGINE</span></i></Link><br></br>
+            <Link href="./Battery" className="rty1" ><i> <FontAwesomeIcon icon={faSquare} className="rty"/><span className='cvb'>BATERY</span></i></Link><br></br>
+            <Link href="./Wheel" className="rty1" ><i> <FontAwesomeIcon icon={faSquare} className="rty"/><span className='cvb'>WHEEL</span></i></Link><br></br>
+            <Link href="./Fuel" className="rty1" ><i> <FontAwesomeIcon icon={faSquare} className="rty"/><span className='cvb'>FUEL TANK</span></i></Link><br></br>
+            <Link href="./Screen" className="rty1" ><i> <FontAwesomeIcon icon={faSquare} className="rty"/><span className='cvb'>WINDSCREEN</span></i></Link>
           </ul>
           </div>
       </div> 
