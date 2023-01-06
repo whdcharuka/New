b217a2185fc40370a866e40bbbb0f2f2d15f1c6d
Dasuni Charuka <dasunicharuka4@gmail.com>
2023-01-06 08:19:21 +0530

    new

---
+++ pages/Components/edu/SlideCard.js
@@ -0,0 +1,48 @@
+import Image from 'next/image'
+import Head from 'next/head';
+import service from '/public/images/Customer-Service.jpg'
+import styles from '/styles/Services.module.css'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faArchive, faHeadphones, faThumbsUp, faTruck } from "@fortawesome/free-solid-svg-icons";
+import Navbar from '../Navbar/Navbar';
+import Footer from '../Footer';
+import Link from 'next/link';
+import Sdata from "./Sdata"
+
+const SlideCard = () => {
+  const settings = {
+    dots: true,
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    appendDots: (dots) => {
+      return <ul style={{ margin: "0px" }}>{dots}</ul>
+    },
+  }
+  return (
+    <>
+      {/* <Slider {...settings}> */}
+        {Sdata.map((value, index) => {
+          return (
+            <>
+              <div className='box d_flex top' key={index}>
+                <div className='left'>
+                <h3 className='baw'>Education</h3>
+                  <h6>{value.title}</h6>
+                  {/* <p>{value.desc}</p> */}
+                  <Link href='./Bottle' className='abc'><button className='btn-primary1'>VISIT COLLECTIONS</button></Link>
+                </div>
+                <div className='right'>
+                  <img src={value.cover} alt='' />
+                </div>
+              </div>
+            </>
+          )
+        })}
+      {/* </Slider> */}
+    </>
+  )
+}
+
+export default SlideCard
